9c0351186035713111fb425ce08a9fdf1411c634
kateleenchar <kateleen.charlorin@polymtl.ca>
2025-01-21 16:46:45 -0500

    exo1

--- exo1.py
+++ exo1.py
@@ -1,14 +1,13 @@
 # Demander le nom complet de l'utilisateur
-print("Veuillez entrer votre nom complet:")
-nom_complet = input()
+print('veuilez entrer votre nom complet')
+mon_nom_complet: input()
+
 # Demander l'âge de l'utilisateur
-print("Veuillez entrer votre âge:")
-âge = int(input())
+
 # Définir l'année actuelle
 année_actuelle = 2025
 # Calculer l'année de naissance
-année_de_naissance = année_actuelle - âge
+année_actuelle
 # Afficher un message de bienvenue avec le nom complet
-print("Bonjour", nom_complet)
+print('bonjour', mon_nom_complet)
 # Afficher l'année de naissance
-print("Vous êtes né(e) en", année_de_naissance)
