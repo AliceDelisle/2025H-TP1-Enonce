b191b2375dfa603d06f98124f29047f6b4340a93
kateleenchar <kateleen.charlorin@polymtl.ca>
2025-01-22 11:53:48 -0500

    exo2

--- exo2.py
+++ exo2.py
@@ -1,23 +1,21 @@
 # Demandez à l'utilisateur d'entrer le niveau de charge actuel de la batterie
 print('veuillez entrée votre niveau de batterie.')
-niveau_de_batterie= int(input())
+niveau_de_batterie = int(input())
 # Vérifiez si le niveau de charge est valide
-if 0 <=niveau_de_baterie  <= 100:
-    'niveau de baterie est valide'
-else: 'niveau de baterie est invalide'
+if 0 <= niveau_de_batterie <= 100:
+    print('niveau de baterie est valide')
+else: print('niveau de baterie est invalide')
 # Arrondir le pourcentage à la dizaine la plus proche
 niveau_de_batterie_arondi= round(niveau_de_batterie,-1)
 # Calculer le nombre de barres
 nombre_de_barres = niveau_de_batterie_arondi // 10
 # Afficher la représentation graphique de la charge de la batterie
-espace= ''
 for i in range(1,2):
-    if niveau_de_baterie_arondi %10 == 0:
-        print('[+ str(nombre_de_barre)* ❚ espace + ]\n' + niveau_de_baterie + '%')
+    if niveau_de_batterie_arondi % 10 == 0:
+        print('['+ nombre_de_barres* '❚' + (10- nombre_de_barres)* '' + ']')
         
-    else: print('['(nombre_de_barre%10)*espace +']\n' + niveau_de_baterie + '%') 
      
-
+print(niveau_de_batterie + '%')
 
 
     
